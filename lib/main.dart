fe9ac80d02cfda7219a9612de432eb18ef890f0f
yoonki-kim <yoonki.kim71@gmail.com>
2021-10-16 13:03:33 +0900

    2021-10-16; 기본 레이아웃 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,25 +12,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.menu),
-            title: Text('title'),
-            actions: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    print('search');
-                  }),
-              IconButton(
-                  icon: Icon(Icons.select_all),
-                  onPressed: () {
-                    print('select');
-                  })
-            ],
-          ),
-          body: Center(child: Text('body'))),
+    return MaterialApp(home: home());
+  }
+
+  Widget home() {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.menu),
+          title: Text('Title메인'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  print('search');
+                }),
+            IconButton(
+                icon: Icon(Icons.select_all),
+                onPressed: () {
+                  print('select');
+                })
+          ],
+        ),
+        body: body());
+  }
+
+  Widget body() {
+    return Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(color: Colors.lime),
+        child: col());
+  }
+
+  Widget col() {
+    return Column(
+      children: [row(), row(), row(), row(), Text('aaaa')],
+    );
+  }
+
+  Widget row() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Text('111'),
+        Text('222'),
+        Text('333'),
+        Text('444'),
+        Text('555'),
+        Text('111'),
+        Text('222'),
+        Text('333'),
+        Text('444'),
+        Text('555')
+      ],
     );
   }
 }
