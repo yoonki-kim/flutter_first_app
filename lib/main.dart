b13846d2b557d46af27b45fb02dd1b9fc1d7ca7c
yoonki-kim <yoonki.kim71@gmail.com>
2021-10-16 14:31:08 +0900

    2021-10-16; StatefulWidget 동적화면 구성

--- lib/main.dart
+++ lib/main.dart
@@ -3,59 +3,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: home());
+    return MaterialApp(home: Home());
   }
+}
 
-  Widget home() {
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int i = 0;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text('Title메인'),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  print('search');
-                }),
-            IconButton(
-                icon: Icon(Icons.select_all),
-                onPressed: () {
-                  print('select');
-                })
-          ],
-        ),
-        body: body());
+        appBar: AppBar(title: Text('Title'), actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  i++;
+                });
+              },
+              icon: Icon(Icons.security))
+        ]),
+        body: Name(i: i));
   }
+}
 
-  Widget body() {
-    return Container(
-        padding: const EdgeInsets.all(20),
-        // decoration: const BoxDecoration(color: Colors.lime),
-        child: testlist());
-  }
+class Name extends StatelessWidget {
+  const Name({Key? key, this.i = 0}) : super(key: key);
+  final int i;
 
-  Widget testlist() {
-    List<String> items = List.generate(20, (index) => "Item $index");
-    // for (var i = 0; i < 10; i++) {
-    //   items.add('bbbb');
-    // }
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(items[index]),
-          subtitle: Text('subtitle sample ...'),
-        );
-      },
-    );
+  @override
+  Widget build(BuildContext context) {
+    if (i % 3 == 0) {
+      return CircularProgressIndicator();
+    } else if (i % 3 == 1) {
+      return Icon(Icons.security);
+    } else {
+      return Text('clicked $i');
+    }
   }
 }
